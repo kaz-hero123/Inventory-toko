6b433958ca8eb5aea0ee1abf750404d0c4ef03c3
hilman-nidal-hamzi <hilmanhamzi@gmail.com>
2026-06-22 11:55:00 +0700

    feat(models): define Category eloquent model and relations

---
+++ app/Models/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    /**
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+}
